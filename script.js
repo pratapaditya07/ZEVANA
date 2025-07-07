7d04eb59b067b3a059839101667252c351c8e823
pratapaditya07 <139495535+pratapaditya07@users.noreply.github.com>
2025-07-07 18:13:51 +0530

    added the scroll

--- script.js
+++ script.js
@@ -33,6 +33,47 @@ gsap.from("#nav_right", {
 });
 
 
+/* function scrollRight() {
+document.getElementById("scrollArea").scrollBy({ left: 255, behavior: 'smooth' });
+}
+
+function scrollLeft() {
+document.getElementById("scrollArea").scrollBy({ left: -255, behavior: 'smooth' });
+}
+
+
+function highlightCenterItem() {
+const container = document.getElementById("scrollArea");
+const items = container.querySelectorAll(".item");
+const containerCenter = container.scrollLeft + container.offsetWidth / 2;
+
+let closestItem = null;
+let closestDistance = Infinity;
+
+items.forEach(item => {
+    const itemCenter = item.offsetLeft + item.offsetWidth / 2;
+    const distance = Math.abs(containerCenter - itemCenter);
+    
+    if (distance < closestDistance) {
+    closestDistance = distance;
+    closestItem = item;
+    }
+
+    // Reset all
+    item.style.transform = "translateY(0px)";
+});
+
+  // Lower the center one
+if (closestItem) {
+    closestItem.style.transform = "translateY(20px)";
+}
+}
+
+document.getElementById("scrollArea").addEventListener("scroll", () => {
+clearTimeout(window.scrollTimeout);
+window.scrollTimeout = setTimeout(highlightCenterItem, 100);
+}); */
+
 
 
 
